ae10755dcd3a7cf286fd664d21262013be90bbc3
ilonapavlenko <112856540+ilonapavlenko@users.noreply.github.com>
2023-05-21 13:30:42 +0300

    Update form.php

--- 6/form.php
+++ 6/form.php
@@ -109,6 +109,7 @@
                 }
             ?>
         </table>
+        <?php if (!empty($_SESSION['login'])) {echo '<input type="hidden" name="token" value="' . $_SESSION["token"] . '">'; } ?>
     </form>
 </body>
 </html>
