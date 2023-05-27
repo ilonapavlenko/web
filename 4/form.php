b9a45d2f4b60e21e514e6cef7f840def1067fe74
ilonapavlenko <112856540+ilonapavlenko@users.noreply.github.com>
2023-05-27 14:52:04 +0300

    Update form.php

--- 4/form.php
+++ 4/form.php
@@ -5,7 +5,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js" defer></script>
-  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="style4.css">
   <title>4</title>
 </head>
 <body>
