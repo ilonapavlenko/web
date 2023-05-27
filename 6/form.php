0ef70aa18fd0fb5b541c63c2e6e5afde57d5a3d6
ilonapavlenko <112856540+ilonapavlenko@users.noreply.github.com>
2023-05-27 15:49:58 +0300

    Update form.php

--- 6/form.php
+++ 6/form.php
@@ -9,23 +9,18 @@
     <link rel="stylesheet" href="style5.css">
 </head>
 <body>
-<?php
-if (!empty($messages)) {
-  print('<div id="messages">');
+<div class="col col-10 col-md-6" id="forma">
+    <?php
+  if (!empty($messages)) {
+    print('<div id="messages">');
   // Выводим все сообщения.
-  foreach ($messages as $message) {
-    print($message);
+    foreach ($messages as $message) {
+      print($message);
   }
-
-
   print('</div>');
 }
-
-// Далее выводим форму отмечая элементы с ошибками классом error
-// и задавая начальные значения элементов ранее сохраненными.
-?>    
-
-<?php
+    ?>
+    <?php
 if (!empty($_COOKIE[session_name()]) && !empty($_SESSION['login'])){
     echo '
         <div class = "login">
@@ -44,87 +39,54 @@ else
     </form>
     </div>
 ';
-?>
-    <form id="form" action="" method="POST">
-        <table>
-            <caption>Данные формы</caption>
-            <tr> 
-                <th>id</th>
-                <th>Имя</th>
-                <th>email</th>
-                <th>Дата рождения</th>
-                <th>Пол</th>
-                <th>Количество конечностей</th>
-                <th>Суперсила</th>
-                <th>Биография</th>
-            </tr>
-            <?php
-                foreach ($values as $value) {
-                    echo    '<tr>
-                            <td style="font-weight: 700;">'; print($value['application_id']); echo '</td>
-                            <td>
-                                <input class="input" name="fio'.$value['application_id'].'" value="'; print(htmlspecialchars($value['fio'])); echo '">
-                            </td>
-                            <td>
-                                <input class="input" name="email'.$value['application_id'].'" value="'; print(htmlspecialchars($value['email'])); echo '">
-                            </td>
-                            <td>
-                            <input class="input" name="date'.$value['application_id'].'" value="'; print($value['date']); echo '">
-                            </td>
-                            <td> 
-                                <div class="column-item">
-                                    <input type="radio" id="g1'.$value['application_id'].'" name="sex'.$value['application_id'].'" value="m" '; if (htmlspecialchars($value['sex']) == 'm') echo 'checked'; echo '>
-                                    <label for="g1'.$value['application_id'].'">Мужчина</label>
-                                </div>
-                                <div class="column-item">
-                                    <input type="radio" id="g2'.$value['application_id'].'" name="sex'.$value['application_id'].'" value="w" '; if (htmlspecialchars($value['sex']) == 'w') echo 'checked'; echo '>
-                                    <label for="g2'.$value['application_id'].'">Женщина</label>
-                                </div>
-                            </td>
-                            <td>
-                                <div class="column-item">
-                                Количество конечностей:
-                                    <input type="radio" id="l1'.$value['application_id'].'" name="limbs'.$value['application_id'].'" value="1" '; if (htmlspecialchars($value['limbs'] == '1')) echo 'checked'; echo '>
-                                    <label for="l1'.$value['application_id'].'">1</label>
-                                </div>
-                                <div class="column-item">
-                                    <input type="radio" id="l2'.$value['application_id'].'" name="limbs'.$value['application_id'].'" value="left" '; if (htmlspecialchars($value['limbs'] == '2')) echo 'checked'; echo '>
-                                    <label for="radioLeft'.$value['application_id'].'">2</label>
-                                </div>
-                            </td>';
-                    $stmt = $db->prepare("SELECT powers_id FROM abilities WHERE application_id = ?");
-                    $stmt->execute([$value['application_id']]);
-                    $abilities = $stmt->fetchAll(PDO::FETCH_COLUMN);
-                    echo    '<td class="abilities">
-                                <div class="column-item">
-                                    <input type="checkbox" id="god'.$value['application_id'].'" name="abilities'.$value['application_id'].'[]" value="1"' . (in_array(1, $abilities) ? ' checked' : '') . '>
-                                    <label for="god'.$value['application_id'].'">бессмертие</label>
-                                </div>
-                                <div class="column-item">
-                                    <input type="checkbox" id="noclip'.$value['application_id'].'" name="abilities'.$value['application_id'].'[]" value="2"' . (in_array(2, $abilities) ? ' checked' : '') . '>
-                                    <label for="noclip'.$value['application_id'].'">прохождение сквозь стены</label>
-                                </div>
-                                <div class="column-item">
-                                    <input type="checkbox" id="levitation'.$value['application_id'].'" name="abilities'.$value['application_id'].'[]" value="3"' . (in_array(3, $abilities) ? ' checked' : '') . '>
-                                    <label for="levitation'.$value['application_id'].'">левитация</label>
-                                </div>
-                            </td>
-                            <td>
-                                <textarea name="bio'.$value['application_id'].'" id="" cols="30" rows="4" maxlength="128">'; print (htmlspecialchars($value['bio']); echo '</textarea>
-                            </td>
-                            <td>
-                                <div class="column-item">
-                                    <input name="save'.$value['application_id'].'" type="submit" value="save'.$value['application_id'].'"/>
-                                </div>
-                                <div class="column-item">
-                                    <input name="clear'.$value['application_id'].'" type="submit" value="clear'.$value['application_id'].'"/>
-                                </div>
-                            </td>
-                        </tr>'; 
-                }
-            ?>
-        </table>
-        <?php if (!empty($_SESSION['login'])) {echo '<input type="hidden" name="token" value="' . $_SESSION["token"] . '">'; } ?>
-    </form>
-</body>
+        <form id="form" action="" method="POST">
+            <div class="group">
+                <label for="name">Имя:
+                <input name="fio" id="name" class="form-control <?php if ($errors['fio']) {print 'error';} ?>" placeholder="Введите ваше имя" value="<?php print $values['fio']; ?>" ></label>
+            </div>
+            <div class="group">
+                <label for="email">E-mail:
+                <input name="email" type="email" id="email" class="form-control <?php if ($errors['email']) {print 'error';} ?>" placeholder="Введите вашу почту" value="<?php print $values['email']; ?>">
+                </label>
+            </div>
+            <div class="group">
+                Дата рождения:
+                <input name="date" type="date" class="form-control <?php if ($errors['date']) {print 'error';} ?>" value="<?php print $values['date']; ?>" />
+            </div>
+            <div class="group">
+                Пол:
+                <label for="g1"><input type="radio" class="form-check-input <?php if ($errors['sex']) {print 'error';} ?>" name="sex" id="g1" value="m" <?php if ($values['sex']=='m') {print 'checked';} ?>>
+                    Мужской</label>
+                <label for="g2"><input type="radio" class="form-check-input <?php if ($errors['sex']) {print 'error';} ?>" name="sex" id="g2" value="w" <?php if ($values['sex']=='w') {print 'checked';} ?>>
+                    Женский</label>
+            </div>
+            <div class="group">
+                Количество конечностей:
+                <label for="l1"><input type="radio" class="form-check-input <?php if ($errors['limbs']) {print 'error';} ?>" name="limbs" id="l1" value="1" <?php if ($values['limbs']=='1') {print 'checked';} ?>>
+                    1</label>
+                <label for="l2"><input type="radio" class="form-check-input <?php if ($errors['limbs']) {print 'error';} ?>" name="limbs" id="l2" value="2" <?php if ($values['limbs']=='2') {print 'checked';} ?>>
+                    2</label>
+                <label for="l3"><input type="radio" class="form-check-input <?php if ($errors['limbs']) {print 'error';} ?>" name="limbs" id="l3" value="3" <?php if ($values['limbs']=='3') {print 'checked';} ?>>
+                    3</label>
+                <label for="l4"><input type="radio" class="form-check-input <?php if ($errors['limbs']) {print 'error';} ?>" name="limbs" id="l4" value="4" <?php if ($values['limbs']=='4') {print 'checked';} ?>>
+                    4</label>
+            </div>
+            <div class="group">
+                <label for="powers">Сверхспособности:
+                <select class="form-control <?php if ($errors['abilities']) {print 'error';} ?>" name="abilities[]" id="powers" multiple="multiple">
+                    <option value="1" <?php if(!empty($values['abilities'][0])) {if ($values['abilities'][0]=='1') {print 'selected';}} ?>>бессмертие</option>
+                    <option value="2" <?php if(!empty($values['abilities'][1])) {if ($values['abilities'][1]=='2') {print 'selected';}} ?>>прохождение сквозь стены</option>
+                    <option value="3" <?php if(!empty($values['abilities'][2])) {if ($values['abilities'][2]=='3') {print 'selected';}} ?>>левитация</option>
+                </select></label>
+            </div>
+            <div class="group">
+                <label for="bio">Биография:
+                <textarea name="bio" id="bio" rows="3" class="form-control <?php if ($errors['bio']) {print 'error';} ?>"><?php print $values['bio']; ?></textarea></label>
+            </div>
+            <label><input type="checkbox" class="form-check-input <?php if ($errors['checkbox']) {print 'error';} ?>" id="checkbox" value="1" name="checkbox" <?php if ($values['checkbox']=='1') {print 'checked';} ?>>
+                C контрактом ознакомлен(а) </label><br>
+            <input type="submit" id="end" class="btn btn-primary" value="Отправить">
+        </form>
+  </div>
+</body> 
 </html>
