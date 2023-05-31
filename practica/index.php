0038a2ea7382832c36f6c2a505c427563b9c3049
ilonapavlenko <112856540+ilonapavlenko@users.noreply.github.com>
2023-05-31 14:49:16 +0300

    Create index.php

---
+++ practica/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta fio="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
+    <link rel="stylesheet" href="style/index.css">
+    <link type="image/x-icon" href="logo.png" rel="shortcut icon">
+    <link type="Image/x-icon" href="logo.png" rel="icon">
+    <title>Holding</title>
+</head>
+<body>
+    <header>
+        <div class="header-items">
+            <a href="#" class="logo">
+                <img src="C:\Users\ilona\Downloads\logo.png" alt="logo" width="100" height="80">
+                <h1>Холдинг&nbsp;&nbsp;</h1>
+            </a>
+            
+        </div>
+    </header>
+    <main>
+        <div class="main-item">
+            <nav>
+                <ul>
+                    <li><a href="Region.php">Список регионов</a></li>
+                    <li><a href="Company.php">Список предприятий</a></li>
+                    <li><a href="Indicators.php">Список показателей</a></li>
+                    <li><a href="Journal.php">Журнал учета отчетных данных</a></li>
+                </ul>
+            </nav>
+            
+        </div>
+        
+    </main>
+</body> 
+</html>
