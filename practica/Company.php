ca84ad4f4badb3af5138224dde0b5692b9850212
ilonapavlenko <112856540+ilonapavlenko@users.noreply.github.com>
2023-05-31 14:51:06 +0300

    Create Company.php

---
+++ practica/Company.php
@@ -0,0 +1,78 @@
+<?php
+
+include('dbconnect.php');
+
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    try {
+        $stmt = $db->prepare("SELECT id, name FROM Company");
+        $stmt->execute();
+        $values = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    } catch (PDOException $e) {
+        print('Error : ' . $e->getMessage());
+        exit();
+    }
+    $new = array();
+    $new['name'] = empty($_COOKIE['name']) ? '' : $_COOKIE['name'];
+    include('assets/Company.php');
+} else {
+    $errors = array();
+    $messages = array();
+    if (!empty($_POST['addnewdate'])) {
+        if (empty($_POST['name'])) {
+            $errors['name1'] = 'Заполните поле "Название компании"';
+            setcookie('name', '', time() + 24 * 60 * 60);
+        } else if (!preg_match('/^[\p{L}\p{M}\s.]+$/u', $_POST['name'])) {
+            $errors['name2'] = 'Некорректно заполнено поле "Название компании"';
+            setcookie('name', $_POST['name'], time() + 24 * 60 * 60);
+        } 
+        
+        if (empty($errors)) {
+            $name = $_POST['name'];
+            $stmt = $db->prepare("INSERT INTO Company (name) VALUES (?, ?)");
+            $stmt->execute([$name, $city]);
+            $messages['added'] = 'Компания "'.$name.'" успешно добавлена';
+            setcookie('name', '', time() + 24 * 60 * 60);
+        }
+    } 
+    foreach ($_POST as $key => $value) {
+        if (preg_match('/^clear(\d+)_x$/', $key, $matches)) {
+            $id = $matches[1]; 
+            $stmt = $db->prepare("SELECT id FROM Journal WHERE CompanyID = ?");
+            $stmt->execute([$id]);
+            $empty = $stmt->rowCount() === 0;
+            if (!$empty) {
+                $errors['delete'] = 'Поле с <b>id = '.$id.'</b> невозможно удалить, т.к. оно связанно с журналом учета';
+            } else {
+                $stmt = $db->prepare("DELETE FROM Company WHERE id = ?");
+                $stmt->execute([$id]);
+                $messages['deleted'] = 'Компания с <b>id = '.$id.'</b> успешно удалена';
+            }
+        }
+        if (preg_match('/^edit(\d+)_x$/', $key, $matches)) {
+            $id = $matches[1];
+            setcookie('edit', $id, time() + 24 * 60 * 60);
+        }
+        if (preg_match('/^save(\d+)_x$/', $key, $matches)) {
+            setcookie('edit', '', time() + 24 * 60 * 60);
+            $id = $matches[1];
+            $stmt = $db->prepare("SELECT name FROM Company WHERE id = ?");
+            $stmt->execute([$id]);
+            $old_dates = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+            $dates['name'] = $_POST['name' . $id];
+
+            if (array_diff_assoc($dates, $old_dates[0])) {
+                $stmt = $db->prepare("UPDATE Company SET name = ? WHERE id = ?");
+                $stmt->execute([$dates['name'], $dates['city'],  $id]);
+                $messages['edited'] = 'Компания с <b>id = '.$id.'</b> успешно обновлена';
+            }
+        }
+    }
+    if (!empty($messages)) {
+        setcookie('messages', serialize($messages), time() + 24 * 60 * 60);
+    }
+    if (!empty($errors)) {
+        setcookie('errors', serialize($errors), time() + 24 * 60 * 60);
+    }
+    header('Location: Company.php');
+}
