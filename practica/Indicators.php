52a2099861223ff1ef321fab35759f3300ffc765
ilonapavlenko <112856540+ilonapavlenko@users.noreply.github.com>
2023-05-31 15:15:40 +0300

    Update Indicators.php

--- practica/Indicators.php
+++ practica/Indicators.php
@@ -59,3 +59,128 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
         print('Error : ' . $e->getMessage());
         exit();
     }
+    
+
+    $new = array();
+    $new['name'] = empty($_COOKIE['name']) ? '' : $_COOKIE['name'];
+    $new['revenue'] = empty($_COOKIE['revenue']) ? '' : $_COOKIE['revenue'];
+    $new['city'] = empty($_COOKIE['city']) ? '' : $_COOKIE['city'];
+    include('assets/Indicators.php');
+} else {
+    $errors = array();
+    $messages = array();
+    if (!empty($_POST['addnewdate'])) {
+        if (empty($_POST['name'])) {
+            $errors['name1'] = 'Заполните поле "Название предприятия"';
+            setcookie('name', '', time() + 24 * 60 * 60);
+        } else if (!preg_match('/^[\p{L}\p{M}\s.]+$/u', $_POST['name'])) {
+            $errors['name2'] = 'Некорректно заполнено поле "Название предприятия"';
+            setcookie('name', $_POST['name'], time() + 24 * 60 * 60);
+        } else {
+            setcookie('name', $_POST['name'], time() + 24 * 60 * 60);
+        }
+
+        if (empty($_POST['revenue'])) {
+            $errors['revenue1'] = 'Заполните поле "Прибыль"';
+            setcookie('revenue', '', time() + 24 * 60 * 60);
+        } else if (!is_numeric($_POST['revenue'])) {
+            $errors['revenue2'] = 'Некорректно заполнено поле "Прибыль"';
+            setcookie('revenue', $_POST['revenue'], time() + 24 * 60 * 60);
+        } else {
+            setcookie('revenue', $_POST['revenue'], time() + 24 * 60 * 60);
+        }
+
+        if (empty($_POST['city'])) {
+            $errors['city1'] = 'Заполните поле "Город"';
+            setcookie('city', '', time() + 24 * 60 * 60);
+        } else if (!preg_match('/^[\p{L}\p{M}\s.]+$/u', $_POST['city'])) {
+            $errors['city2'] = 'Некорректно заполнено поле "Город"';
+            setcookie('city', $_POST['city'], time() + 24 * 60 * 60);
+        } else {
+            setcookie('city', $_POST['city'], time() + 24 * 60 * 60);
+        }
+        
+        if (empty($errors)) {
+            $name = $_POST['name'];
+            $revenue = intval($_POST['revenue']);
+            $city = $_POST['city'];
+            $stmt = $db->prepare("INSERT INTO Indicators (name, revenue, city) VALUES (?, ?, ?)");
+            $stmt->execute([$name, $revenue, $city]);
+            $messages['added'] = 'Показатель "'.$name.'" успешно добавлен';
+            setcookie('name', '', time() + 24 * 60 * 60);
+            setcookie('revenue', '', time() + 24 * 60 * 60);
+            setcookie('city', '', time() + 24 * 60 * 60);
+        }
+    } 
+    foreach ($_POST as $key => $value) {
+        if (preg_match('/^clear(\d+)_x$/', $key, $matches)) {
+            $id = $matches[1]; 
+            $stmt = $db->prepare("SELECT id FROM Journal WHERE IndicatorsID = ?");
+            $stmt->execute([$id]);
+            $empty = $stmt->rowCount() === 0;
+            if (!$empty) {
+                $errors['delete'] = 'Поле с <b>id = '.$id.'</b> невозможно удалить, т.к. оно связанно с журналом учета';
+            } else {
+                $stmt = $db->prepare("DELETE FROM Indicators WHERE id = ?");
+                $stmt->execute([$id]);
+                $messages['deleted'] = 'Показатель с <b>id = '.$id.'</b> успешно удалён';
+            }
+        }
+        if (preg_match('/^edit(\d+)_x$/', $key, $matches)) {
+            $id = $matches[1];
+            setcookie('edit', $id, time() + 24 * 60 * 60);
+        }
+        if (preg_match('/^save(\d+)_x$/', $key, $matches)) {
+            setcookie('edit', '', time() + 24 * 60 * 60);
+            $id = $matches[1];
+            $stmt = $db->prepare("SELECT name, revenue, city FROM Indicators WHERE id = ?");
+            $stmt->execute([$id]);
+            $old_dates = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+            $dates['name'] = $_POST['name' . $id];
+            $dates['revenue'] = $_POST['revenue' . $id];
+            $dates['city'] = $_POST['city' . $id];
+
+            if (array_diff_assoc($dates, $old_dates[0])) {
+                $stmt = $db->prepare("UPDATE Indicators SET name = ?, revenue = ?, city = ? WHERE id = ?");
+                $stmt->execute([$dates['name'], $dates['revenue'], $dates['city'], $id]);
+                $messages['edited'] = 'Показатель с <b>id = '.$id.'</b> успешно обновлён';
+            }
+        }
+    }
+
+    if (!empty($_POST['resetall'])) {
+        setcookie('revenues', '');
+        setcookie('cities', '');
+    }
+
+    if (!empty($_POST['filter'])) {
+
+        $filter_revenue_ids = array();
+        foreach ($_POST as $key => $value) {
+            if (strpos($key, 'filter_revenue_') !== false) {
+                $id = substr($key, 11);
+                $filter_revenue_ids[] = $id;
+            }
+        }
+        setcookie('revenues', serialize($filter_revenue_ids));
+
+        $filter_city_ids = array();
+        foreach ($_POST as $key => $value) {
+            if (strpos($key, 'filter_city_') !== false) {
+                $id = substr($key, 15);
+                $filter_city_ids[] = $id;
+            }
+        }
+        setcookie('cities', serialize($filter_city_ids));
+        
+    }
+
+    if (!empty($messages)) {
+        setcookie('messages', serialize($messages), time() + 24 * 60 * 60);
+    }
+    if (!empty($errors)) {
+        setcookie('errors', serialize($errors), time() + 24 * 60 * 60);
+    }
+    header('Location: Indicators.php');
+}
